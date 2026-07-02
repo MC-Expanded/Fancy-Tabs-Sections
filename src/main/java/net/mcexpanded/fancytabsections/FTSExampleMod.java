b5e54053cf2a818f1367ae21c00301c2129ce8ba
wd <lucasalvesbispo2@gmail.com>
2026-07-02 16:02:00 +0100

    v3.0 release

--- src/main/java/net/mcexpanded/fancytabsections/FTSExampleMod.java
+++ src/main/java/net/mcexpanded/fancytabsections/FTSExampleMod.java
@@ -32,7 +32,7 @@ public class FTSExampleMod
     public FTSExampleMod(IEventBus modEventBus)
     {
         //prevents example mod from running adding stuff
-        //if (true) return;
+        if (true) return;
 
         //register our creative mode tabs as usual, leaving them empty
         FTSExampleCreativeModeTabs.register(modEventBus);
